dc60153748451bcc1cf86990d9134b3271dfe45c
Adam Mustapha <matawalle4u@gmail.com>
2026-01-20 23:37:32 +0100

    Fixed argument type

--- inventory-client/src/app.service.ts
+++ inventory-client/src/app.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AppService {
   constructor(@Inject('INVENTORY_SERVICE') private readonly client: ClientProxy) {}
-  getTotalItems(numbers): Observable<number> {
+  getTotalItems(numbers: number[]): Observable<number> {
     return this.client.send({ cmd: 'sum' }, numbers);
   }
 }
